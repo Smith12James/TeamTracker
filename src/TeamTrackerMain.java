0928e4f78571cc5f725443d09c2c34d1e8f97900
James Smith <smith12james12@gmail.com>
2021-02-28 15:49:49 -0700

    created a new repository for TeamTracker

---
+++ src/TeamTrackerMain.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * This application is designed to read a webpage about the Utah jazz, return player/team stats,
+ * and display what the users are looking for, wherther that be team stats or player stats in Java
+ *
+ * @author James Smith
+ *
+ */
+
+
+
+public class TeamTrackerMain {
+
+    public static void main(String args[]) {
+
+        // Where I will call other classes/methods to print player info to the console
+
+    }
+
+}
